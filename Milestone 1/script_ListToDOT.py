6665a4bdf2f85512ecde8dd6aff076be11d79acb
saimwani <saimdawood@gmail.com>
2019-02-14 02:01:19 +0530

    added script for printing DOT file and test file used  (called test)

---
+++ Milestone 1/script_ListToDOT.py
@@ -0,0 +1,50 @@
+
+
+file1 = open("treeGen.txt","w")#write mode
+#file1 = open(outputHtml,"a")
+file1.write("digraph graphname {")
+file1.write("\n")
+
+
+
+
+
+counter=0
+def writeGraph(someList):
+    global counter
+    local=counter
+    counter+=1
+    name=someList[0]
+    if(len(someList) > 1):
+        for innerList in someList[1:]:
+            if(len(innerList) >0):
+                file1.write(str(local))
+                file1.write ("[label=\"")
+                file1.write (name)
+                file1.write ("\" ] ;")
+
+                file1.write(str(counter))
+                file1.write ("[label=\"")
+                if ((innerList[0][0])=="\""):
+                    innerList[0]=innerList[0][1:-1]
+                file1.write (innerList[0])
+                file1.write ("\" ] ;")
+                file1.write(str(local) + "->" + str(counter) + ";")
+
+                file1.write("\n")
+                writeGraph(innerList)
+
+
+
+alist=['SourceFile', ['main'], ['ImportDecl_curl', ['ImportDecl_curl', ['ImportDecl_curl', [], ['"fmt"']], ['"math"']], ['ImportSpec_curl', ['ImportSpec_curl', [], ['"tensor"']], ['"torch"']]]]
+writeGraph(alist)
+file1.write("}")
+file1.close()
+
+"""digraph graphname {
+    1[label="sdas"];
+     1 -> 2 -> 3;
+     b -> d;
+     v -> eee;
+ }
+"""
