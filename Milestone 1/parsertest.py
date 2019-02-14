a8722b6b98076817bfc4f2751df288bb9a13bd41
saimwani <saimdawood@gmail.com>
2019-02-15 03:56:11 +0530

    for clause error

--- Milestone 1/parsertest.py
+++ Milestone 1/parsertest.py
@@ -1217,10 +1217,10 @@ def p_ForClause(p):
     """
     p[0]=['ForClause']
     for index in range(1,len(p)):
-        if(isinstance(p[index],str) and p[index]!="(" and p[index]!=";"):
-            p[0].append([p[index]])
-        elif (not isinstance(p[index], str)):
-            p[0].append(p[index])
+      if(isinstance(p[index],str) and p[index]!="(" and p[index]!=")"):
+        p[0].append([p[index]])
+      else:
+        p[0].append(p[index])
 
 def p_RangeClause(p):
     """
@@ -1326,4 +1326,4 @@ file1.close()
 outputTree=sys.argv[1]+".ps"
 
 import os
-os.system("dot -Tps treeGen.txt -o" + outputTree)
+os.system("dot -Tps treeGen.txt -o" +outputTree)
