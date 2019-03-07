0bb8857385d980621eb5055b02fda1d8a83a3a9b
Yash <ymahajan98@gmail.com>
2019-03-08 02:28:34 +0530

    Fixed indentation

--- Mile/src/symTab.py
+++ Mile/src/symTab.py
@@ -1,19 +1,19 @@
 class symbolTable:
     def __init__(self):
-	self.table={}
-	self.parent=None
-
-	def search(self,ident):
-		return (self.table.get(ident))
-
-	def insert(self,ident,attribute):
-		if(not self.search(ident)):
-			(self.table)[ident]={}
-			self.table[ident]["type"]=attribute
-
-	def updateList(self,ident,key,value):
-		if(self.search(ident)):
-			(self.table)[ident][key]=value
-
+        self.table={}
+        self.parent=None
+    
+    def search(self,ident):
+        return (self.table.get(ident))
+    
+    def insert(self,ident,attribute):
+        if(not self.search(ident)):
+    	    (self.table)[ident]={}
+    	    self.table[ident]["type"]=attribute
+    
+    def updateList(self,ident,key,value):
+        if(self.search(ident)):
+            (self.table)[ident][key]=value
+    
     def assignParent(self, parent):
         self.parent=parent
