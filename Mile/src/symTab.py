82c37f748fd4fcd5e9095f6475f65ba962df50e7
saimwani <saimdawood@gmail.com>
2019-03-07 22:34:52 +0530

    symbol table

--- Mile/src/symTab.py
+++ Mile/src/symTab.py
@@ -1,17 +1,19 @@
-class symbolTable
-
-	def __init__(self):
-		self.table={}
-		self.parent=None
+class symbolTable:
+    def __init__(self):
+	self.table={}
+	self.parent=None
 
 	def search(self,ident):
 		return (ident in self.table)
 
-	def insert(self,ident,attributes):
-		if(not search(self,ident)):
-			self.table[ident]={}
-			self.table[ident]['type']=attributes
+	def insert(self,ident,attribute):
+		if(not self.search(ident)):
+			(self.table)[ident]={}
+			self.table[ident]["type"]=attribute
 
-	def update(self,ident,key,value):
-		if(search(self,ident)):
-			self.table[ident][key]=value
+	def updateList(self,ident,key,value):
+		if(self.search(ident)):
+			(self.table)[ident][key]=value
+
+    def assignParent(self, parent):
+        self.parent=parent
