07ed4eeaefe2a72403aba3703154958eefb849f3
saimwani <saimdawood@gmail.com>
2019-04-07 02:41:28 +0530

    added floating operation

--- Mile/src/genASM.py
+++ Mile/src/genASM.py
@@ -18,10 +18,15 @@ for i in content:
 regToVar=[]
 for i in range(0,28):
     regToVar.append("free")
+regToVarFloat=[]
+for i in range(0,32):
+    regToVarFloat.append("free")
 
 varToReg={}
+varToRegFloat={}
 
 regReplace=1
+regReplaceFloat=0
 
 def getOffset(name):
     for x in scopeTab:
@@ -29,16 +34,28 @@ def getOffset(name):
             continue
         return x.table[name]["offset"]
 
-def getReg():
-    for i in range(1, 28):
-        if (regToVar[i]=="free"):
-            return i
-    off=getOffset(regToVar[regReplace])
-    f.write("sw " + "$"+ str(regReplace) + "," + "-"+str(off)+"($fp)\n")
-    del varToReg[regToVar[regReplace]]
-    org=regReplace
-    regReplace=(regReplace%27) + 1
-    return org
+def getReg(a=None):
+    if (a==None):
+        for i in range(1, 28):
+            if (regToVar[i]=="free"):
+                return i
+        off=getOffset(regToVar[regReplace])
+        f.write("sw " + "$"+ str(regReplace) + "," + "-"+str(off)+"($fp)\n")
+        del varToReg[regToVar[regReplace]]
+        org=regReplace
+        regReplace=(regReplace%27) + 1
+        return org
+    else:
+        for i in range(0, 32):
+            if (regToVarFloat[i]=="free"):
+                return i
+        off=getOffset(regToVarFloat[regReplaceFloat])
+        f.write("swc1 " + "$f"+ str(regReplaceFloat) + "," + "-"+str(off)+"($fp)\n")
+        del varToRegFloat[regToVarFloat[regReplaceFloat]]
+        org=regReplaceFloat
+        regReplaceFloat=(regReplaceFloat+1)%32
+        return org
+
 
 
 
@@ -63,22 +80,44 @@ for code in codeLines:
                     val=1
                 elif(val=="False"):
                     val=0
-
                 reg=getReg()
-                f.write("addi "+"$"+str(reg)+",$0," + str(val))
+                f.write("addi "+"$"+str(reg)+",$0," + str(val)+"\n")
                 regToVar[reg]=code[0]
                 varToReg[code[0]]=reg
 
+
             else:   #float operation
-                xx=0
+                op=code[3][-5]
+                val=eval(code[2]+op+code[4])
+                if (val=="True"):
+                    val=1
+                elif(val=="False"):
+                    val=0
+                reg=getReg(f)
+                f.write("addi "+"$"+str(reg)+",$0," + str(val)+"\n")
+                regToVarFloat[reg]=code[0]
+                varToRegFloat[code[0]]=reg
 
-        #if(code[2][0]!='t' and code[2][0]!='v' and code[4][0]=='t'):      # first constant second temp
 
-        #if(code[2][0]!='t' and code[2][0]!='v' and code[4][0]='v'):    # first constant second vartemp
+        elif(code[2][0]!='t' and code[2][0]!='v' and code[4][0]=='t'):      # constant, temp
+                if (code[3][-2]=="n"):  #integer op
+                    if(varToReg.get(code[4])==None):
+                        reg2=getReg()
+                        off=getOffset(code[4])
+                        f.write("lw " + "$"+ str(reg) + "," + "-"+str(off)+"($fp)\n")
+                    else:
+                        reg2=varToReg[code[4]]
+                    reg1=getReg()
+                    f.write("addi " + "$"+ str(reg1) + ",$0," +code[2]+"\n")
 
-        #if(code[2][0]=='t' and code[4][0]!='v' and code[4][0]!='t'):       # first temp second constant
 
-        #if(code[2][0]=='t' and code[4][0]=='t'):  # first temp secoand temp
+
+
+        #if(code[2][0]!='t' and code[2][0]!='v' and code[4][0]='v'):    # constant, vartemp
+
+        #if(code[2][0]=='t' and code[4][0]!='v' and code[4][0]!='t'):       # temp, constant
+
+        #if(code[2][0]=='t' and code[4][0]=='t'):  # temp, temp
 
         #if(code[2][0]=='t' and code[4][0]=='v' ):  # temp, vartemp
 
