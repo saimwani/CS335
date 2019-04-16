4f30556dec71d37719d6bfbec0e357764dd764c1
Yash <ymahajan98@gmail.com>
2019-04-16 18:00:44 +0530

    Two test cases

--- Mile/src/testing/multi_level_pointer.go
+++ Mile/src/testing/multi_level_pointer.go
@@ -1,5 +1,17 @@
 package main
 var a int=3
 func main(){
-    print a
+    var a0 *****int 
+    var a1 ****int
+    var a2 ***int
+    var a3 **int
+    var a4 *int
+    a4=&a
+    a3=&a4
+    a2=&a3
+    a1=&a2 
+    a0=&a1
+
+    *****a0=4
+    print a,"\n"
 }
